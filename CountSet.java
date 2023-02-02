34b8de551bd6a5b78568bc2845c3200dce68836d
takealook <takealook97@naver.com>
2023-02-02 16:30:18 +0900

    CountSet 클래스 생성

---
+++ CountSet.java
@@ -0,0 +1,35 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class CountSet {
+    final List<Integer> A = Arrays.asList(1, 1, 2, 2, 3, 3);
+    final List<Integer> B = Arrays.asList(1, 3, 3, 3);
+
+    List<Integer> append(int input) {
+
+    }
+
+    List<Integer> remove(int input) {
+
+    }
+
+    int countFor(int input) {
+
+    }
+
+    List<Integer> sum(List<Integer> input) {
+
+    }
+
+    List<Integer> complement(List<Integer> input) {
+
+    }
+
+    List<Integer> intersect(List<Integer> input) {
+
+    }
+
+    Integer[] getResult(List<Integer> input) {
+        return input.toArray(new Integer[0]);
+    }
+}
