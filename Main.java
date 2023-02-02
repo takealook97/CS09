8350c5222d2ce3ce1521842752a775bf24d4c9b1
takealook <takealook97@naver.com>
2023-02-02 16:29:53 +0900

    출력 수정

--- Main.java
+++ Main.java
@@ -1,10 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         Output output = new Output();
-        output.printA();
-        output.printB();
-        output.printSum();
-        output.printComplement();
-        output.printIntersect();
+        output.printSquadSet();
     }
 }
--- Output.java
+++ Output.java
@@ -1,22 +1,16 @@
 public class Output {
-    void printA(){
+    void printSquadSet() {
         SquadSet squadset = new SquadSet();
+        System.out.println("---SquadSet---");
         System.out.println("A 집합 = " + squadset.A);
-    }
-    void printB(){
-        SquadSet squadset = new SquadSet();
         System.out.println("B 집합 = " + squadset.B);
-    }
-    void printSum(){
-        SquadSet squadset = new SquadSet();
         System.out.println("합집합 = " + squadset.getSum());
-    }
-    void printComplement(){
-        SquadSet squadset = new SquadSet();
         System.out.println("차집합 = " + squadset.getComplement());
-    }
-    void printIntersect(){
-        SquadSet squadset = new SquadSet();
         System.out.println("교집합 = " + squadset.getIntersect());
+    }
+
+    void printCountSet() {
+        CountSet countset = new CountSet();
+
     }
 }
